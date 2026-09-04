ee5d400c677d5e3bfdcb2a6890a7e5c66c661cd3
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-09-04 22:01:30 +0300

    Add sending messagies

--- UserService/src/main/java/io/github/lilacbud/userservice/service/UserServiceImpl.java
+++ UserService/src/main/java/io/github/lilacbud/userservice/service/UserServiceImpl.java
@@ -3,19 +3,24 @@ package io.github.lilacbud.userservice.service;
 import io.github.lilacbud.userservice.dto.UserDTO;
 import io.github.lilacbud.userservice.mappers.UserMapper;
 import io.github.lilacbud.userservice.models.User;
+import io.github.lilacbud.userservice.models.UserMessage;
 import io.github.lilacbud.userservice.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private static final String TOPIC = "user_events";
     private final UserRepository repository;
     private final UserMapper mapper;
+    private final KafkaTemplate<String, UserMessage> kafkaTemplate;
     
     @Override
     @Transactional(readOnly = true)
@@ -27,16 +32,23 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void deleteUserById(Long id) {
+        Optional<User> foundUser = repository.findById(id);
         repository.deleteById(id);
         System.out.println("Deleting was successfully called");
+        foundUser.ifPresent(user -> {
+            UserMessage message = new UserMessage(UserMessage.UserEvent.USER_DELETED, user.getEmail());
+            kafkaTemplate.send(TOPIC, message);
+        });
     }
 
     @Override
     @Transactional
     public UserDTO saveUser(UserDTO dto) {
-        UserDTO result = mapper.mapToUserDTO(repository.save(mapper.mapToUserEntity(dto)));
+        User user = repository.save(mapper.mapToUserEntity(dto));
         System.out.println("Saving was successfully called for " + dto);
-        return result;
+        UserMessage message = new UserMessage(UserMessage.UserEvent.USER_CREATED, user.getEmail());
+        kafkaTemplate.send(TOPIC, message);
+        return mapper.mapToUserDTO(user);
     }
 
     @Override
@@ -58,7 +70,12 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void deleteAllUsers() {
+        Iterable<User> users = repository.findAll();
         repository.deleteAll();
         System.out.println("Deleting was successfully called");
+        users.forEach(user -> {
+            UserMessage message = new UserMessage(UserMessage.UserEvent.USER_DELETED, user.getEmail());
+            kafkaTemplate.send(TOPIC, message);
+        });
     } 
 }
