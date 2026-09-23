5eec9dbe5ddf30b58334419a4ba33db14213479c
lilac-bud <169933042+lilac-bud@users.noreply.github.com>
2026-09-23 13:43:01 +0300

    Fix FallbackController

--- ProjectGateway/src/main/java/io/github/lilacbud/projectgateway/controller/FallbackController.java
+++ ProjectGateway/src/main/java/io/github/lilacbud/projectgateway/controller/FallbackController.java
@@ -2,14 +2,13 @@ package io.github.lilacbud.projectgateway.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
-    @GetMapping("/message")
+    @RequestMapping("/message")
     public ResponseEntity<String> getFallbackMessage() {
         final String message = "Service is currently unavailable. Please, retry later.";
         return ResponseEntity
